f767388ca195e04e15dc03a57b2e4e131c0e4e39
Mathius <mathiuskormasela12@gmail.com>
2023-04-04 20:58:16 +0700

    create the required tables

--- src/core/Database.ts
+++ src/core/Database.ts
@@ -1,5 +1,3 @@
-/* eslint-disable no-async-promise-executor */
-/* eslint-disable @typescript-eslint/no-misused-promises */
 // ========== Database
 // import all modules
 import mysql, { type QueryError, type Connection } from 'mysql2'
@@ -13,7 +11,7 @@ class Database {
       password: config?.database?.password
     })
 
-    return await new Promise((resolve, reject) => {
+    return new Promise((resolve, reject) => {
       const sql: string = `CREATE DATABASE IF NOT EXISTS \`${config?.database?.name ?? 'test'}\` CHARACTER SET utf8 COLLATE utf8_general_ci;`
       db.query(sql, (err: QueryError) => {
         if (err !== null) {
@@ -32,18 +30,42 @@ class Database {
       host: config?.database?.host,
       user: config?.database?.user,
       password: config?.database?.password,
-      database: config?.database?.name
+      database: config?.database?.name,
+      multipleStatements: true
     })
 
-    return await new Promise((resolve, reject) => {
+    return new Promise((resolve, reject) => {
       const sql: string = `
 				CREATE TABLE IF NOT EXISTS users(
 					id INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
 					name VARCHAR(255) NOT NULL,
 					email VARCHAR(255) NOT NULL UNIQUE,
-					createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+					createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 					updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 				);
+
+				CREATE TABLE IF NOT EXISTS rooms(
+					id INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+					name VARCHAR(255) NOT NULL,
+					createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+					updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+				);
+
+				CREATE TABLE IF NOT EXISTS chats(
+					id INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+					roomId INT(11) NOT NULL,
+					userId INT(11) NOT NULL,
+					message VARCHAR(255) NOT NULL,
+					createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+					updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+
+					FOREIGN KEY (roomId) REFERENCES rooms(id),
+					FOREIGN KEY (userId) REFERENCES users(id)
+				);
+
+				CREATE INDEX usersIndex ON users (id, email);
+				CREATE INDEX roomsIndex ON rooms (id);
+				CREATE INDEX chatsIndex ON chats (id);
 			`
       db.query(sql, (err: QueryError) => {
         if (err !== null) {
@@ -57,22 +79,21 @@ class Database {
     })
   }
 
-  public async sync (): Promise<string> {
-    return await new Promise(async (resolve, reject) => {
-      try {
-        await this.createDatabase()
+  public async sync (): Promise<void> {
+    try {
+      await this.createDatabase()
 
-        try {
-          await this.createTables()
-          resolve('The database has been created')
-        } catch (err) {
-          const error = err as { message: string }
-          reject(new Error(`The database has been created, but failed to be created tables because of ${String(error.message.toLowerCase())}`))
-        }
+      try {
+        await this.createTables()
+        console.log('The database has been created')
       } catch (err) {
-        reject(err)
+        const error = err as { message: string }
+        console.error(error.message)
       }
-    })
+    } catch (err) {
+      const error = err as { message: string }
+      console.error(error.message)
+    }
   }
 
   public get db (): Connection {
@@ -80,7 +101,8 @@ class Database {
       host: config?.database?.host,
       user: config?.database?.user,
       password: config?.database?.password,
-      database: config?.database?.name
+      database: config?.database?.name,
+      multipleStatements: true
     })
   }
 }
